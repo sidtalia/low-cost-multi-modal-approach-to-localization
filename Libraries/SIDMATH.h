2d3fcf5cec93847eb5c592e79e5d542778d81a8b
naughtyStark <sidharthtalia@ymail.com>
2018-12-14 19:38:05 +0530

    Update SIDMATH.h

--- Libraries/SIDMATH.h
+++ Libraries/SIDMATH.h
@@ -6,39 +6,8 @@
 
 #define DEG2RAD (float)0.0174533
 #define RAD2DEG (float)57.2958
-
-class coordinates //for storing multiple coordinates as we further progress to multiple waypoint trajectories instead of just 1 waypoint
-{                               //this will become the main point of focus once the basic tasks are complete
-  float longitude,latitude,X,Y;
-  public:
-  float slope;
-  void setcords(float y,float x)
-  {
-    latitude=y;
-    longitude=x;
-  }
-  float rety()
-  {
-    return latitude;
-  }
-  float retx()
-  {
-    return longitude;
-  }
-  float setcordsO(float y,float x)
-  {
-    X=x;
-    Y=y;
-  }
-  float retYO()
-  {
-    return Y;
-  }
-  float retXO()
-  {
-    return X;
-  }
-};
+#define DEG2METER (double)111392.84
+#define METER2DEG (double)1/DEG2METER
 
 float anglecalcy(float x1,float x2,float y1,float y2);  
 
@@ -57,14 +26,6 @@ float my_asin(float a);
 float my_cos(float a);
 
 float my_sin(float a);
-
-float get_T(float V,float X1,float Y1,float X2,float Y2,float X3,float Y3,float X4,float Y4,float d,float dt);
- 
-float ROC(float X1,float Y1,float X2,float Y2,float X3,float Y3,float X4,float Y4,float t,float max_Acceleration);
-
-void get_Intermediate_Points(float slope1,float slope2,float X1,float X2,float Y1,float Y2,float d);
-
-void generate_Slopes(coordinates c[],int n) ;
 
 float spike(float mean, float x);
 
