872be277d925907c9b5e2548ad9d59880d93617e
naughtyStark <sidharthtalia@ymail.com>
2019-02-06 09:15:49 +0530

    Update PARAMS.h

--- Libraries/PARAMS.h
+++ Libraries/PARAMS.h
@@ -2,9 +2,13 @@
 #define _PARAMS_H_
 
 #define dt (float)0.0025
+#define LOOP_FREQUENCY 400
 #define dt_micros 2500
 
-#define ride_height (float)0.062 //6.2cms height of the sensor.
+#define ride_height (float)0.06 //6.2cms height of the sensor.
+#define DIST_BW_ACCEL_AXLE (float)0.225// distance between rear axle and accelerometer
+#define THROTTLE_OFFSET 1535
+#define DECLINATION (float)-1.5
 
 #define MODE_STOP 0x00
 #define MODE_STANDBY 0X01
@@ -23,9 +27,10 @@
 #define OFFSET_ID 0x01
 #define COMMAND_ID 0X02
 
-#define CONTROL_FREQUENCY 200
+#define CONTROL_FREQUENCY LOOP_FREQUENCY/2
 #define FUTURE_TIME (float)2/CONTROL_FREQUENCY
-#define CONTROL_TIME (int)1000/CONTROL_FREQUENCY //control time in ms
+#define CONTROL_TIME (float)1000/CONTROL_FREQUENCY //control time in ms
+
 #define WP_CIRCLE 0.5 //1/2 meter radius around waypoint.
 
 #define WP_ID 0x0005
